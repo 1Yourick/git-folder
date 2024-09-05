8cd33200647da169151fd3b821aecc901c65609d
Andrei Yurkov <dev.yourick@gmail.com>
2024-09-05 18:10:35 +0300

    сортировка по датевремени

--- my_lessons.py
+++ my_lessons.py
@@ -1,45 +1,26 @@
-# '''Вам доступен именованный кортеж User, который содержит данные о пользователе некоторого ресурса.
-# Первым элементом именованного кортежа является имя пользователя, вторым — фамилия, третьим — адрес электронной почты,
-# четвертым — статус оформленной подписки. Также доступен список users, содержащий эти кортежи.
-#
-# Дополните приведенный ниже код, чтобы он вывел данные о каждом пользователе из этого списка,
-# предварительно отсортировав их по статусу подписки от дорогой к дешевой, а при совпадении
-# статусов — в лексикографическом порядке адресов электронных почт. Данные о каждом пользователе
-# должны быть указаны в следующем формате:'''
-# <имя> <фамилия>
-#  Email: <адрес электронной почты>
-#  Plan: <статус подписки>
+# У Тимура имеется немало друзей из других городов или стран, которые часто приезжают к нему
+# в гости с целью увидеться и развлечься. Чтобы не забыть ни об одной встрече, Тимур записывает
+# имена и фамилии друзей в csv файл, дополнительно указывая для каждого дату и время встречи.
+# Вам доступен этот файл, имеющий название meetings.csv, в котором в первом столбце записана фамилия,
+# во втором — имя, в третьем — дата в формате DD.MM.YYYY , в четвертом — время в формате HH:MM:
+# Напишите программу, которая выводит фамилии и имена друзей Тимура, предварительно отсортировав
+# их по дате и времени встречи от самой ранней до самой поздней. Фамилии и имена должны быть расположены каждые на отдельной строке.
 
+import csv
+from datetime import datetime
 from collections import namedtuple
 
-User = namedtuple('User', ['name', 'surname', 'email', 'plan'])
+with open('meetings.csv', encoding='utf-8') as csv_file:
+    headers = csv_file.readline()
+    rows = csv.reader(csv_file)
+    frends_list = list()
 
-users = [User('Mary', 'Griffin', 'sonnen@yahoo.com', 'Basic'),
-         User('Brenda', 'Young', 'retoh@outlook.com', 'Silver'),
-         User('Kathleen', 'Lyons', 'balchen@att.net', 'Gold'),
-         User('Pamela', 'Hicks', 'corrada@sbcglobal.net', 'Silver'),
-         User('William', 'Townsend', 'kosact@verizon.net', 'Gold'),
-         User('Clayton', 'Morris', 'berserk@yahoo.com', 'Silver'),
-         User('Dorothy', 'Dennis', 'sequin@live.com', 'Gold'),
-         User('Tyler', 'Walker', 'noahb@comcast.net', 'Basic'),
-         User('Joseph', 'Moore', 'ylchang@sbcglobal.net', 'Silver'),
-         User('Kenneth', 'Richardson', 'tbusch@me.com', 'Bronze'),
-         User('Stephanie', 'Bush', 'neuffer@live.com', 'Gold'),
-         User('Gregory', 'Hughes', 'juliano@att.net', 'Basic'),
-         User('Tracy', 'Wallace', 'sblack@me.com', 'Silver'),
-         User('Russell', 'Smith', 'isaacson@comcast.net', 'Bronze'),
-         User('Megan', 'Patterson', 'hoangle@outlook.com', 'Basic')]
+    # Friends = namedtuple('Friends', headers)
+    for row in rows:
+        d = datetime.strptime(row[2]+'_'+row[3], '%d.%m.%Y_%H:%M')
+        frends_list.append(
+            [row[0] + ' ' + row[1], d])
 
-
-subscribe = {
-    'Gold': 0,
-    'Silver': 1,
-    'Bronze': 2,
-    'Basic': 3
-}
-users = sorted(users, key=lambda x: (subscribe[x.plan], x.email))
-for user in users:
-    print(user.name, user.surname)
-    print('  Email:', user.email)
-    print('  Plan:', user.plan)
-    print()
+    frends_list = sorted(frends_list, key=lambda x: x[1])
+for f in frends_list:
+    print(f[0])
