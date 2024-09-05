5e9238034a6b4861a15d7dadc1edf200bcc94109
Andrei Yurkov <dev.yourick@gmail.com>
2024-09-05 16:28:37 +0300

    6.4 именнованный кортеж, интересная сортировка

--- my_lessons.py
+++ my_lessons.py
@@ -1,16 +1,45 @@
-
-#Вам доступен именованный кортеж Animal, который содержит данные о животном. Первым элементом именованного кортежа является имя животного, вторым — семейство, третьим — пол, четвертым — цвет. Также доступен файл data.pkl, содержащий сериализованный список таких кортежей.
-#Дополните приведенный ниже код, чтобы для каждого кортежа из этого списка он вывел названия его полей и значения этих полей в следующем формате:
-
+# '''Вам доступен именованный кортеж User, который содержит данные о пользователе некоторого ресурса.
+# Первым элементом именованного кортежа является имя пользователя, вторым — фамилия, третьим — адрес электронной почты,
+# четвертым — статус оформленной подписки. Также доступен список users, содержащий эти кортежи.
+#
+# Дополните приведенный ниже код, чтобы он вывел данные о каждом пользователе из этого списка,
+# предварительно отсортировав их по статусу подписки от дорогой к дешевой, а при совпадении
+# статусов — в лексикографическом порядке адресов электронных почт. Данные о каждом пользователе
+# должны быть указаны в следующем формате:'''
+# <имя> <фамилия>
+#  Email: <адрес электронной почты>
+#  Plan: <статус подписки>
 
 from collections import namedtuple
-import pickle
 
-Animal = namedtuple('Animal', ['name', 'family', 'sex', 'color'])
+User = namedtuple('User', ['name', 'surname', 'email', 'plan'])
 
-with open('data.pkl', 'rb') as pfile:
-    file = pickle.load(pfile)
-    for line in file:
-        for field, value in zip(Animal._fields,line):
-            print(f'{field}: {value}')
-        print()
+users = [User('Mary', 'Griffin', 'sonnen@yahoo.com', 'Basic'),
+         User('Brenda', 'Young', 'retoh@outlook.com', 'Silver'),
+         User('Kathleen', 'Lyons', 'balchen@att.net', 'Gold'),
+         User('Pamela', 'Hicks', 'corrada@sbcglobal.net', 'Silver'),
+         User('William', 'Townsend', 'kosact@verizon.net', 'Gold'),
+         User('Clayton', 'Morris', 'berserk@yahoo.com', 'Silver'),
+         User('Dorothy', 'Dennis', 'sequin@live.com', 'Gold'),
+         User('Tyler', 'Walker', 'noahb@comcast.net', 'Basic'),
+         User('Joseph', 'Moore', 'ylchang@sbcglobal.net', 'Silver'),
+         User('Kenneth', 'Richardson', 'tbusch@me.com', 'Bronze'),
+         User('Stephanie', 'Bush', 'neuffer@live.com', 'Gold'),
+         User('Gregory', 'Hughes', 'juliano@att.net', 'Basic'),
+         User('Tracy', 'Wallace', 'sblack@me.com', 'Silver'),
+         User('Russell', 'Smith', 'isaacson@comcast.net', 'Bronze'),
+         User('Megan', 'Patterson', 'hoangle@outlook.com', 'Basic')]
+
+
+subscribe = {
+    'Gold': 0,
+    'Silver': 1,
+    'Bronze': 2,
+    'Basic': 3
+}
+users = sorted(users, key=lambda x: (subscribe[x.plan], x.email))
+for user in users:
+    print(user.name, user.surname)
+    print('  Email:', user.email)
+    print('  Plan:', user.plan)
+    print()
