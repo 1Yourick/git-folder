7b87b5960efa31dd3c89c3faf0ca13906fe80bed
Andrei Yurkov <dev.yourick@gmail.com>
2024-10-09 22:53:48 +0300

    кнопки работают

--- tm_bot.py
+++ tm_bot.py
@@ -1,9 +1,10 @@
 import telebot
 from telebot import types
-import datetime
+from datetime import date, timedelta, time
 import locale
 #для перевода дней недели
 locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
+date_pattern = "%d.%m.%Y"
 
 token = '7762712132:AAGzzk_k5eOp_FDPn0B9ZJaUZoH7seo6pn8'
 bot = telebot.TeleBot(token)
@@ -14,29 +15,23 @@ Hello, I`m bot. I will help you to do something'''
 #Стартовое сообщение
 @bot.message_handler(commands=['start', 'help'])
 def main(message):
-   
+    
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("key1")
-    btn2 = types.KeyboardButton("key2")
-    btn3 = types.KeyboardButton("key3")
+    btn1 = types.KeyboardButton("Свободные окошки")
+    btn2 = types.KeyboardButton("Записаться на процедуру")
     markup.add(btn1)
     markup.add(btn2)
-    markup.add(btn3)
     
     bot.send_message(message.chat.id, START_MESSAGE, reply_markup=markup)
 
-@bot.message_handler(commands=['key1'])
-def button_message(message):
-    bot.send_message(message.chat.id, 'hi 1')
-@bot.message_handler(commands=['key2'])
-def button_message(message):
-    bot.send_message(message.chat.id, 'hi 2')
-@bot.message_handler(commands=['key3'])
-def button_message(message):
-    bot.send_message(message.chat.id, 'hi 3')
-
-week_list = list()
-for i in range(8):
-    week_list.append((datetime.date.today() + datetime.timedelta(days=i)).strftime('%d.%m.%Y %A'))
+# Выбор функции после нажатия на кнопки
+@bot.message_handler(content_types=['text'])
+def get_message(message):
+    if message.text == 'Свободные окошки':
+        bot.send_message(message.chat.id, 'Мест НЕТ !')  
+    elif message.text == 'Записаться на процедуру':
+        bot.send_message(message.chat.id, 'Окей, записываемся на процедуру')
+        bot.send_message(message.chat.id, f'Сегодня {date.today().strftime("%d.%m.%Y %A")}')
+  
 
 bot.polling(non_stop=True)
