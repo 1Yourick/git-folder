b0be74b6b66611eabfd40d5b6ea1401c6e30f680
Andrei Yurkov <dev.yourick@gmail.com>
2025-06-01 21:58:58 +0300

    добавление и просмотр учеников

--- eng_teach_bot.py
+++ eng_teach_bot.py
@@ -1,5 +1,6 @@
 import telebot
 from telebot import types
+import json
 
 token = '8066145939:AAElzyCi2vv0VgzREzDXyu3wyzFndxv2oBo'
 PASSWORD = '222'
@@ -10,7 +11,16 @@ msg_hello_message = 'Hello dear teacher'
 msg_wrong_password = 'Wrong password'
 states = {}
 students = []
+path = 'git folder/students.json'
 
+
+def open_json(path = 'git folder/students.json'):
+    with open(path, 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+def save_json(json_dict, path = 'git folder/students.json'):
+    with open(path, 'w', encoding='utf-8') as file:
+        json.dump(json_dict,file, ensure_ascii=False)
 
 #First message
 @bot.message_handler(commands=['start', 'help'])
@@ -28,6 +38,7 @@ def authorization(message):
     else:
         bot.send_message(message.chat.id, msg_wrong_password)
 
+
 #send buttons
 def first_buttons(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -38,15 +49,52 @@ def first_buttons(message):
 
     bot.send_message(message.chat.id,text = msg_hello_message, reply_markup=markup)
 
-@bot.message_handler(commands=['My students 📖'])
+# two func for show students
+@bot.message_handler(func=lambda message: states.get(message.chat.id) == "student list", content_types=['text'])
 def show_students(message):
-    pass
-#TODO отображение студентов
+    json_dict = open_json()
+    if message.text == 'Add lesson':
+        pass
+    elif message.text == 'Remove lesson':
+        pass
+    elif message.text not in json_dict.keys():
+        states[message.chat.id] = 'auth_done'
+        bot.send_message(message.chat.id, 'Student not found')
+    else:
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        #TODO вывод сообщения с информацией об уроках.
+        INFO_TEXT = f'''Здесь будет иинформация об уроках(словарях)'''
+        btn1 = types.KeyboardButton('Add lesson')
+        btn2 = types.KeyboardButton('Remove lesson')
+        markup.add(btn1)
+        markup.add(btn2)
+        bot.send_message(message.chat.id, INFO_TEXT, reply_markup=markup)
 
-@bot.message_handler(commands=['Add new student'])
+    
+@bot.message_handler(func=lambda message: message.text == 'My students 📖', content_types=['text'])
+def show_students(message):
+    states[message.chat.id] = 'student list'
+    #show buttons with students
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    json_dict = open_json()
+    for user in json_dict:
+        btn = types.KeyboardButton(user)
+        markup.add(btn)
+    bot.send_message(message.chat.id, 'Students list:', reply_markup=markup)
+
+# two func for add students
+@bot.message_handler(func=lambda message: states.get(message.chat.id) == "add student", content_types=['text'])
 def add_students(message):
-    pass
-#TODO отображение студентов
+    #add new student in json
+    json_file = open_json()
+    json_file[message.text] = dict()
+    save_json(json_file)
 
+    bot.send_message(message.chat.id, f'Student {message.text} added in students list')
+    states[message.chat.id] = 'auth_done'
+@bot.message_handler(func=lambda message: message.text == 'Add new student', content_types=['text'])
+def add_students(message):
+    states[message.chat.id] = 'add student' 
+    bot.send_message(message.chat.id, 'Write student name\nExample: Michael Jackson')
 
 bot.polling(non_stop=True, timeout=123)
